4724b13687aaacee4b86f0a7c43ed0bfd3508e68
Kunal <kunalkale292@gmail.com>
2024-12-06 17:58:19 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Home from './Componants/Home'
+import About from './Componants/About'
+import Contact from './Componants/Contact'
+import Services from './Componants/Services'
+import MyNavBar from './Componants/MyNavBar'
+import "./App.CSS"
+import MyCounter from './Componants/MyCounter'
+import MyDialog from './Componants/MyDialog'
+
+const App = () => {
+  return (
+    <>
+    
+    <MyDialog/>
+    <MyCounter/>
+  
+{/* 
+      <MyNavBar/>
+    <section id ='home' className='home-sec'>
+      <Home/>
+    </section>
+
+    <section id ='about'>
+      <About/>
+    </section>
+
+    <section id ='contact'>
+      <Contact/>
+    </section>
+
+    <section id ='services'>
+      <Services/>
+    </section> */}
+
+    
+    </>
+  )
+}
+
+export default App
